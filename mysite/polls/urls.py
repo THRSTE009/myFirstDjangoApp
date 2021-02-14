85d3a1a6a44628147e0a293e51fc0766f6944429
Steven Theron <steve.theron365@gmail.com>
2021-02-14 22:15:19 +0200

    Tutorial 3
    1. Added to views.py : detail, results and vote functions.
    2. Created a templates directory for polls.
    3. Added index.html and details.html templates.
    4. Used the template system (using dot-lookup syntax) to access variable attributes in polls/detail.html.
    5. Removed hard-coded URLs in templates to make it easier to change URLs on projects with multiple templates.
    6. Added an app_name 'polls' to the urls.py so that Django knows which app view to create for a URL when using the "{% url %} template tag".

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,11 +2,24 @@ from django.urls import path
 
 from . import views
 
+# If you want to change the URL of the polls detail view to something else,
+# perhaps to something like polls/specifics/12/ instead of doing it in the template (or templates) you would change it in polls/urls.py:
+app_name = 'polls'
+    # How does one make it so that Django knows which app view to create for a url when using the {% url %} template tag?
+    # The answer is to add namespaces to your URLconf. In the polls/urls.py file, go ahead and add an 'app_name' to set the application namespace.
 urlpatterns = [
-    path('', views.index, name='index'), 
-    #path() has four arguments:  (route, view, kwargs, name). The last two are optional.  
-    #route --> is a string that contains a URL pattern.
-    #view --> The specified view function is called with an HttpRequest obejct. E.g. index() inside view.py.
-    #kwargs --> is am arbitrary keyword arguments that can be passed in a dictionary to the target view.
-    #name --> The name that you give the URL.
+
+    path('', views.index, name='index'), # ex: /polls/
+    path('<int:question_id>/', views.detail, name='detail'), # ex: /polls/5/
+        # the 'name' value as called by the {% url %} template tag
+
+    # Using angle brackets �captures� part of the URL and sends it as a keyword argument to the view function.
+    path('<int:question_id>/results/', views.results, name='results'), # ex: /polls/5/results
+    path('<int:question_id>/vote/', views.vote, name='vote'), # ex: /polls/5/vote
+
+    # path() has four arguments:  (route, view, kwargs, name). The last two are optional.
+    # route --> is a string that contains a URL pattern.
+    # view --> The specified view function is called with an HttpRequest obejct. E.g. index() inside view.py.
+    # kwargs --> is am arbitrary keyword arguments that can be passed in a dictionary to the target view.
+    # name --> The name that you give the URL.
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,35 @@
 from django.http import HttpResponse
 
-#To call the view, map it to a URL via the URLconf.
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
+# Views must return either an HttpResponse object, containnig the content for the requested page, or raise an exception, such as Http404.
+
+# To call the view, map it to a URL via the URLconf.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = { 'latest_question_list': latest_question_list } # Context is a dictionary mapping template variable names to Python objects.
+    return render(request, 'polls/index.html', context) # render(request object, template name, dictionary(optional))
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id) #if a question with this idea does not exist then the view will raise the Http404 exception.
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    #faster method to raise Http404 is:
+    question = get_object_or_404(Question, pk=question_id)
+        # get_object_or_404 acts as a helper function.
+        # It's used to decouple the 'model layer' from the 'view layer'. I.e. The model API raises Http404.
+        # get_object_or_404('takes a Django model', 'takes an arbitrary number of keyword arguments')
+        # these arguments are passed to the model's manager get() which raises Http404 if the object does not exist.
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
